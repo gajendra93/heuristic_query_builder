3c9d93388b452377b390dadf9f0cb17f5103032a
gajendra93 <gajendrasp.rj.9693@gmail.com>
2016-12-22 16:44:00 +0530

    Added onclick functionality for all the buttons

--- src/components/QueryPage.jsx
+++ src/components/QueryPage.jsx
@@ -2,17 +2,38 @@ import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 
 export default class QueryPage extends React.Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+        this.addNode = this.addNode.bind(this);
+        this.readNode = this.readNode.bind(this);
+        this.updateNode = this.updateNode.bind(this);
+        this.deleteNode = this.deleteNode.bind(this);
+    }
+    addNode() {
+        this.setState({view: '<AddPage />'});
+    }
+    readNode() {
+        this.setState({view: '<ReadPage />'});
+    }
+    updateNode() {
+        this.setState({view: '<UpdatePage />'});
+    }
+    deleteNode() {
+        this.setState({view: '<DeletePage />'});
+    }
     render(){
         const style = {
             margin: 12
         };
         return (
             <div>
-                <RaisedButton label="Add" primary={true} style={style} />
-                <RaisedButton label="Read" primary={true} style={style} />
-                <RaisedButton label="Update" primary={true} style={style} />
-                <RaisedButton label="Delete" primary={true} style={style} />
+                <RaisedButton label="Add" primary={true} style={style} onClick={this.addNode} />
+                <RaisedButton label="Read" primary={true} style={style} onClick={this.readNode} />
+                <RaisedButton label="Update" primary={true} style={style} onClick={this.updateNode} />
+                <RaisedButton label="Delete" primary={true} style={style} onClick={this.deleteNode} />
+                {this.state.view}
             </div>
         )
     }
